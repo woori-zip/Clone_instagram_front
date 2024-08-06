9b5cc8d799be3e26bf112b6957478389932b5132
dlwnssh28 <dlwnssh28@gmail.com>
2024-08-06 16:42:58 +0900

    라우터 등록

--- src/components/Auth/signin.jsx
+++ src/components/Auth/signin.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from '../styles/auth.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/auth.module.css";
 
 function SignIn() {
   return (
     <div className={`${styles.container} ${styles.mt20}`}>
       <div className={styles.formContainer}>
         <h1 className={`${styles.heading} ${styles.mb20}`}>Instagram</h1>
-        
-        <input className={styles.input} type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-        <input className={styles.input} type="password" placeholder="비밀번호" />
-        
-        <button className={`${styles.btn} ${styles.bgBlue} ${styles.mb10}`}>로그인</button>
+
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="비밀번호"
+        />
+
+        <button className={`${styles.btn} ${styles.bgBlue} ${styles.mb10}`}>
+          로그인
+        </button>
 
         <div className={styles.orSeparator}>
           <div className={styles.line}></div>
@@ -20,20 +30,25 @@ function SignIn() {
         </div>
 
         <button className={`${styles.btn} ${styles.bgWhite} ${styles.mb20}`}>
-          <img 
-            src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" 
-            alt="Facebook logo" 
-            style={{ width: '20px', marginRight: '8px' }}
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+            alt="Facebook logo"
+            style={{ width: "20px", marginRight: "8px" }}
           />
           Facebook으로 로그인
         </button>
-        <Link to='/accounts/password/reset' className={styles.textMuted}>비밀번호를 잊으셨나요?</Link>
+        <Link to="/accounts/password/reset" className={styles.textMuted}>
+          비밀번호를 잊으셨나요?
+        </Link>
       </div>
-      
+
       <div className={`${styles.container} ${styles.mt20}`}>
         <div className={`${styles.formContainer}`}>
-          계정이 없으신가요? <Link to="/accounts/emailsignup" className={styles.link}>가입하기</Link>
-        </div>  
+          계정이 없으신가요?{" "}
+          <Link to="/accounts/emailsignup" className={styles.link}>
+            가입하기
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignIn from '../pages/signin';
-import SignUp from '../pages/signup';
-import PasswordReset from '../pages/passwordreset'
-import Home from '../pages/Home'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "../components/Auth/signin";
+import SignUp from "../components/Auth/signup";
+import PasswordReset from "../components/Auth/passwordreset";
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/accounts/login' element={<SignIn />} />
-        <Route path='/accounts/emailsignup' element={<SignUp />} />
-        <Route path='/accounts/password/reset' element={<PasswordReset />} />
-        <Route path='/' element={<Home />} />
+        <Route path="/accounts/login" element={<SignIn />} />
+        <Route path="/accounts/emailsignup" element={<SignUp />} />
+        <Route path="/accounts/password/reset" element={<PasswordReset />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
