3fe49f7ca1e241e3ab1d938bca209d6b102d1c88
woori <woori.dec@gmail.com>
2024-08-14 21:58:26 +0900

    창 줄였을 때, nav 아래위로 출력되도록 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Sidenav from "../components/sidenav";
 import Timeline from "../components/timeline";
 import styles from "../styles/home.module.css"; // CSS Module import
 import Search from "@mui/icons-material/Search";
+import SmallSideNav from "../components/smallnav";
 
 function Home() {
   const [activeComponent, setActiveComponent] = useState(null); // 초기에는 아무것도 활성화 안함
@@ -13,6 +14,7 @@ function Home() {
 
   return (
     <div className={styles.home_container}>
+      <SmallSideNav handleButtonClick={handleButtonClick} />
       <Sidenav handleButtonClick={handleButtonClick} />
       <div className={styles.timeline_container}>
         <Timeline />
